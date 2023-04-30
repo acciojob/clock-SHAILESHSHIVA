7cf9282321f7e7c3b550935be1019bd80292c578
Shailesh <shaileshmishra9428@gmail.com>
2023-04-30 23:33:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const p = document.getElementById("clock");
+const timer = new Date().toLocaleString();
+
+function timer(){
+	p.innerText = timer;
+}
+
+setInterval(timer,1000);
+
+
